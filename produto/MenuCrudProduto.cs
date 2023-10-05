b22fd33a6c6c5271a18d95769cdb55383d921908
Gustavo Eyroff <janoeyroff@gmail.com>
2023-10-05 19:36:05 -0300

    Inicio do projeto

---
+++ produto/MenuCrudProduto.cs
@@ -0,0 +1,25 @@
+﻿public class ProdutoCRUD
+{
+    private List<Produto> produtos = new List<Produto>();
+
+    public void AdicionarProduto()
+    {
+        Console.WriteLine("oi");
+    }
+
+    public void ListarProdutos()
+    {
+        Console.WriteLine("ola");
+    }
+
+    public void AtualizarProduto()
+    {
+        Console.WriteLine("oii");
+    }
+
+    public void ExcluirProduto()
+    {
+        Console.WriteLine("olaa");
+    }
+}
+
